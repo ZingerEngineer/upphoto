0706300b4aea74c6ca3a75c7fcec639071fb6148
ZingerEngineer <ramyisez@gmail.com>
2023-06-23 04:10:12 +0300

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,36 @@
-import { Routes ,Route} from 'react-router-dom'
-import Home from './pages/Home';
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 import About from './pages/About'
-import NotFound from './pages/NotFound';
-import ProgressList from './components/ProgressList';
-import { Link } from 'react-router-dom';
+import NotFound from './pages/NotFound'
+// import ProgressList from './components/ProgressList';
+// import { Link } from 'react-router-dom';
+import { initializeApp } from 'firebase/app'
+import { firebaseConfig } from './config'
+
 export default function App() {
+  initializeApp(firebaseConfig)
   return (
     <>
-    <div className="App" style={{height : "100%"}}>
-    <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/*' element={<NotFound/>}/>
-    </Routes>
-    </div>
+      <div
+        className="App"
+        style={{ height: '100%' }}
+      >
+        <Routes>
+          <Route
+            path="/"
+            element={<Home />}
+          />
+          <Route
+            path="/about"
+            element={<About />}
+          />
+          <Route
+            path="/*"
+            element={<NotFound />}
+          />
+        </Routes>
+      </div>
     </>
-  );
+  )
 }
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,29 @@
 import NavBar from '../components/NavBar'
 import { ArrowUpOnSquareIcon } from '@heroicons/react/24/outline'
 import ProgressList from '../components/ProgressList'
-import { Firestore, addDoc, collection } from '@firebase/firestore'
-import { db } from '../firebase'
-import { useState, SetStateAction } from 'react'
+import { useState } from 'react'
 
 const Home = () => {
   const [photoToUpload, setPhotoToUpload] = useState<File | null>(null)
+
   // const uploadPhoto = () => {
   //   collection(Firestore, 'temp')
   // }
+
   return (
     <>
       <NavBar />
-      <div className="home-content text-white font-semibold flex flex-col flex-grow">
-        <div className="upload-section w-full flex flex-col items-center bg-gray-900 py-8">
+      <div className="home-content flex flex-grow flex-col font-semibold text-white">
+        <div className="upload-section flex w-full flex-col items-center bg-gray-900 py-8">
           <div className="m-2 flex flex-col items-center">
-            <ArrowUpOnSquareIcon className="w-28 m-4" />
+            <ArrowUpOnSquareIcon className="m-4 w-28" />
             <p>Upload your image or drag it over here.</p>
           </div>
           <div className="flex items-center justify-center">
             <input
               type="file"
               id="files"
-              className="Button px-4 py-2 w-fit h-fit rounded-l-md bg-violet-800 border-0 text-white font-sans font-semibold focus:outline-none hover:cursor-pointer hover:bg-violet-500 duration-100"
+              className="Button h-fit w-fit rounded-l-md border-0 bg-violet-800 px-4 py-2 font-sans font-semibold text-white duration-100 hover:cursor-pointer hover:bg-violet-500 focus:outline-none"
               onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
                 const selectedFile = event.target.files?.[0]
                 setPhotoToUpload(selectedFile || null)
@@ -34,7 +34,7 @@ const Home = () => {
               type="text"
               name="filePath"
               id="filePath"
-              className="block w-full py-2 outline-none rounded-r-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              className="block w-full rounded-r-md border-0 py-1.5 py-2 pl-7 pr-20 text-gray-900 outline-none ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               placeholder="Paste URL here..."
             />
           </div>
