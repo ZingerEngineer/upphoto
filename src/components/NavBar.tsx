c257d0ec8c3add0fc11b688bcfe61a7e7780c30b
ZingerEngineer <ramyisez@gmail.com>
2023-07-20 19:27:40 +0300

    tailwind icon is now a link

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -27,12 +27,10 @@ function classNames(...classes: (boolean | string | undefined)[]): string {
 
 const Example = () => {
   const userData = useContext(userDataContext)
-  const [userName, setUserName] = useState<string | null | undefined>(null)
   const [userEmail, setUserEmail] = useState<string | null | undefined>(null)
   const [userImage, setUserImage] = useState<string | null | undefined>(null)
 
   useEffect(() => {
-    setUserName(userData?.currentUser?.displayName)
     setUserEmail(userData?.currentUser?.email)
     setUserImage(userData?.currentUser?.photoURL)
   }, [userData])
@@ -64,16 +62,26 @@ const Example = () => {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                 <div className="flex flex-shrink-0 items-center">
-                  <img
+                  <Link
+                    to="/"
                     className="block h-8 w-auto lg:hidden"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  />
-                  <img
+                  >
+                    <img
+                      className="h-8 w-auto"
+                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                      alt="Your Company"
+                    />
+                  </Link>
+                  <Link
+                    to="/"
                     className="hidden h-8 w-auto lg:block"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  />
+                  >
+                    <img
+                      className="h-8 w-auto"
+                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                      alt="Your Company"
+                    />
+                  </Link>
                 </div>
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
